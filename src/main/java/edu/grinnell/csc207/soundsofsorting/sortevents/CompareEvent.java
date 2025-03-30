e745e96086de49e5587d65c40476f33be2243d92
Abby Ressner <abbyressner@gmail.com>
2025-03-30 16:20:33 -0500

    implement sortevents classes

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
@@ -1,9 +1,31 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A <code>CompareEvent</code> logs a comparison a sort makes between two
  * indices in the array.
  */
-public class CompareEvent<T> {
-    // TODO: implement me!
+public class CompareEvent<T> implements SortEvent<T> {
+
+    private int index1, index2;
+
+    public CompareEvent(int index1, int index2) {
+        this.index1 = index1;
+        this.index2 = index2;
+    }
+
+    @Override
+    public void apply(T[] arr) {}
+
+    @Override
+    public List<Integer> getAffectedIndices() {
+        return Arrays.asList(index1, index2);
+    }
+
+    @Override
+    public boolean isEmphasized() {
+        return false;
+    }
 }
--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
@@ -1,8 +1,34 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A <code>SwapEvent</code> logs a swap between two indices of the array.
  */
-public class SwapEvent<T> {
-    // TODO: implement me
+public class SwapEvent<T> implements SortEvent<T> {
+
+    private int index1, index2;
+
+    public SwapEvent(int index1, int index2) {
+        this.index1 = index1;
+        this.index2 = index2;
+    }
+
+    @Override
+    public void apply(T[] arr) {
+        T tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+
+    @Override
+    public List<Integer> getAffectedIndices() {
+        return Arrays.asList(index1, index2);
+    }
+
+    @Override
+    public boolean isEmphasized() {
+        return true;
+    }
 }
